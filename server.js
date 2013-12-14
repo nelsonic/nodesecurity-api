77698f3a3e749ee3092dab7ed8b2d554a41422e9
Stephen Rivas <srivas@thuzi.com>
2013-12-14 17:05:14 -0500

    updating server to utlize sarge for role based auth

--- server.js
+++ server.js
@@ -12,9 +12,29 @@ function validate(username, password, callback) {
         }
 
         bcrypt.compare(password, user.password, function (err, isValid) {
-            callback(err, isValid, { id: user.id, user: user });
+            callback(err, isValid, { id: user.id, user: user, role: user.role });
         });
     });
+}
+
+var sarge_config = {
+    handler: function (request, config, next) {
+        var userRole = request.auth.credentials.role,
+            isAuthorized = false;
+
+        // TODO: may need to cast config.roles string to an array
+        // or other sanity checks to ensure it is ['val', 'val'] format
+
+        config.roles.forEach(function (role) {
+            if (userRole === role) isAuthorized = true;
+        });
+
+        if (isAuthorized) {
+            return next();
+        } else {
+            return next(Hapi.error.unauthorized("Not Authorized"));
+        }
+    }
 }
 
 server.auth('simple', {
@@ -26,6 +46,9 @@ server.auth('simple', {
 require('./routes/user')(server);
 require('./routes/aoi')(server);
 require('./routes/report')(server);
+
+// Packs
+server.pack.require('sarge', sarge_config, function () {});
 
 server.start(function () {
     console.log('Server started at: ' + server.info.uri);
